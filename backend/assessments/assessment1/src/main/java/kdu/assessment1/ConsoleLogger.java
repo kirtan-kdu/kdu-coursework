49d8795523c364cad1993160937af1384316707f
kirtan-kdu <kirtanrakeshkumar.shah@kickdrumtech.com>
2024-01-19 12:25:59 +0530

    ipl schedule generator

---
+++ backend/assessments/assessment1/src/main/java/kdu/assessment1/ConsoleLogger.java
@@ -0,0 +1,21 @@
+package kdu.assessment1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsoleLogger {
+    private static final Logger logger = LoggerFactory.getLogger(ConsoleLogger.class);
+
+    public static void warnMethod(String logString){
+        logger.warn(logString);
+    }
+    public static void errorMethod(String logString){
+        logger.error(logString);
+    }
+    public static void infoMethod(String logString){
+        logger.info(logString);
+    }
+    public static void debugMethod(String logString){
+        logger.debug(logString);
+    }
+}
