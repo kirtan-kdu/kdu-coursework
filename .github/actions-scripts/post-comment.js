f3c2eba50c662a07692ca4dd95762c35a5d6d0ca
kirtan-kdu <kirtanrakeshkumar.shah@kickdrumtech.com>
2024-01-22 21:45:27 +0530

    sonar fail fixed

--- .github/actions-scripts/post-comment.js
+++ .github/actions-scripts/post-comment.js
@@ -2,11 +2,7 @@ const axios = require("axios");
 
 // log all the env variables
 console.log(
-<<<<<<< HEAD
-  `PERSONAL_ACCESS_TOKEN_GITHUB: ${process.env.PERSONAL_ACCESS_TOKEN_GITHUB}`
-=======
   `PERSONAL_ACCESS_TOKEN_GITHUB: ${process.env.PERSONAL_ACCESS_TOKEN_GITHUB}`
->>>>>>> e598e992f4f7c5b96118b8129b5d181d3785434e
 );
 console.log(`SONAR_TOKEN: ${process.env.SONAR_TOKEN}`);
 console.log(`SONAR_HOST_URL: ${process.env.SONAR_HOST_URL}`);
@@ -22,15 +18,6 @@ const measuresEndpoint = `${sonarqubeURL}/measures/search`;
 // SonarQube project and metrics details
 const projectKey = process.env.SONAR_PROJECT_KEY;
 const metricKeys = [
-<<<<<<< HEAD
-  "alert_status",
-  "bugs",
-  "code_smells",
-  "vulnerabilities",
-  "reliability_rating",
-  "security_rating",
-  // Add more metric keys as needed
-=======
   "alert_status",
   "bugs",
   "code_smells",
@@ -38,7 +25,6 @@ const metricKeys = [
   "reliability_rating",
   "security_rating",
   // Add more metric keys as needed
->>>>>>> e598e992f4f7c5b96118b8129b5d181d3785434e
 ];
 
 // SonarQube authentication token
@@ -54,16 +40,6 @@ const gitHubAccessToken = process.env.PERSONAL_ACCESS_TOKEN_GITHUB;
 
 // Function to create a comment with a table of metrics on a GitHub issue
 async function createCommentWithMetricsTable(metrics) {
-<<<<<<< HEAD
-  // format metrics as a table
-  const tableHeader = "| Metric | Value |\n| ------ | ----- |";
-  const tableRows = metrics
-    .map(
-      (metric) =>
-        `| ${metric.key} | ${getMetricValue(metric.key, metric.value)} |`
-    )
-    .join("\n");
-=======
   // format metrics as a table
   const tableHeader = "| Metric | Value |\n| ------ | ----- |";
   const tableRows = metrics
@@ -72,106 +48,35 @@ async function createCommentWithMetricsTable(metrics) {
         `| ${metric.key} | ${getMetricValue(metric.key, metric.value)} |`
     )
     .join("\n");
->>>>>>> e598e992f4f7c5b96118b8129b5d181d3785434e
 
-  const commentBody = `${tableHeader}\n${tableRows}`;
+  const commentBody = `${tableHeader}\n${tableRows}`;
 
-  // Create a comment on the GitHub issue
-  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`;
+  // Create a comment on the GitHub issue
+  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`;
 
-  // Axios configuration
-  const axiosConfig = {
-    headers: {
-      Authorization: `Bearer ${gitHubAccessToken}`,
-      Accept: "application/vnd.github.v3+json",
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  };
+  // Axios configuration
+  const axiosConfig = {
+    headers: {
+      Authorization: `Bearer ${gitHubAccessToken}`,
+      Accept: "application/vnd.github.v3+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  };
 
-  try {
-    const response = await axios.post(
-      apiUrl,
-      { body: commentBody },
-      axiosConfig
-    );
-    console.log("Comment created successfully:", response.data);
-  } catch (error) {
-    console.error(
-      "Error creating comment:",
-      error?.response?.data || error.message
-    );
-    throw error;
-  }
-}
-
-function getMetricValue(key, value) {
-  let returnValue = value;
-  switch (key) {
-    case `reliability_rating`:
-      switch (value + "") {
-        case "1.0":
-          returnValue = "A (0 Bugs)";
-          break;
-        case "2.0":
-          returnValue = "B (at least 1 Minor Bug)";
-          break;
-        case "3.0":
-          returnValue = "C (at least 1 Major Bug)";
-          break;
-        case "4.0":
-          returnValue = "D (at least 1 Critical Bug)";
-          break;
-        case "5.0":
-          returnValue = "E (at least 1 Blocker Bug)";
-          break;
-
-        default:
-          break;
-      }
-      returnValue = returnValue + "br/Note: A being best, E being worst";
-      break;
-    case `security_rating`:
-      switch (value + "") {
-        case "1.0":
-          returnValue = "A (0 Vulnerabilities)";
-          break;
-        case "2.0":
-          returnValue = "B (at least 1 Minor Vulnerability)";
-          break;
-        case "3.0":
-          returnValue = "C (at least 1 Major Vulnerability)";
-          break;
-        case "4.0":
-          returnValue = "D (at least 1 Critical Vulnerability)";
-          break;
-        case "5.0":
-          returnValue = "E (at least 1 Blocker Vulnerability)";
-          break;
-
-        default:
-          break;
-      }
-      returnValue = returnValue + "br/Note: A being best, E being worst";
-      break;
-    case `alert_status`:
-      switch (value) {
-        case "OK":
-          returnValue = `b:white_check_mark: ${value}/b`;
-          break;
-        case "ERROR":
-          returnValue = `b:x: ${value}/b`;
-          break;
-
-        default:
-          returnValue = `b:warning: ${value}/b`;
-          break;
-      }
-      break;
-
-    default:
-      break;
-  }
-  return returnValue;
+  try {
+    const response = await axios.post(
+      apiUrl,
+      { body: commentBody },
+      axiosConfig
+    );
+    console.log("Comment created successfully:", response.data);
+  } catch (error) {
+    console.error(
+      "Error creating comment:",
+      error?.response?.data || error.message
+    );
+    throw error;
+  }
 }
 
 function getMetricValue(key, value) {
@@ -246,36 +151,35 @@ function getMetricValue(key, value) {
 
 // Function to fetch metrics data from SonarQube API
 async function fetchMetricsData() {
-  const params = {
-    projectKeys: projectKey,
-    metricKeys: metricKeys.join(),
-  };
+  const params = {
+    projectKeys: projectKey,
+    metricKeys: metricKeys.join(),
+  };
 
-  const axiosConfig = {
-    headers: {
-      Authorization: `Bearer ${sonarToken}`, // Set SonarQube token in the headers
-    },
-  };
+  const axiosConfig = {
+    headers: {
+      Authorization: `Bearer ${sonarToken}`, // Set SonarQube token in the headers
+    },
+  };
 
-  try {
-    const response = await axios.get(measuresEndpoint, {
-      params,
-      ...axiosConfig,
-    });
-    const metrics = response.data.measures.map((metric) => ({
-      key: metric.metric,
-      value: metric.value,
-    }));
-    await createCommentWithMetricsTable(metrics);
-  } catch (error) {
-    console.error(
-      "Error fetching metrics data:",
-      error?.response?.data || error.message
-    );
-    throw error;
-  }
+  try {
+    const response = await axios.get(measuresEndpoint, {
+      params,
+      ...axiosConfig,
+    });
+    const metrics = response.data.measures.map((metric) => ({
+      key: metric.metric,
+      value: metric.value,
+    }));
+    await createCommentWithMetricsTable(metrics);
+  } catch (error) {
+    console.error(
+      "Error fetching metrics data:",
+      error?.response?.data || error.message
+    );
+    throw error;
+  }
 }
 
 // Call the function to fetch metrics data and create a comment with the metrics table
 fetchMetricsData();
-
