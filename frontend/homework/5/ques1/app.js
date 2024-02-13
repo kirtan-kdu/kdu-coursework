ef77bd9471cc1584ac4842496ac8d998d82a797a
kirtan-kdu <kirtanrakeshkumar.shah@kickdrumtech.com>
2024-02-13 18:42:59 +0530

    trying to post on s3

--- frontend/homework/5/ques1/app.js
+++ frontend/homework/5/ques1/app.js
@@ -1,68 +1,64 @@
-const os = require("os")
 const http = require('http');
-const fs = require('fs');
+const express = require("express")
+const app = express();
+const fs = require("fs")
+const detaisRoute = require("./route")
+const {OSDetails, writeJSONToFile} = require("./os-details.js");
+const uploadTOBucket = require('./upload-s3.js');
 
+require("dotenv").config();
 const PORT = 5000;
+app.use(express.json());
+app.set('json spaces', 5); // to pretify json response
 
 
-function getOSDetails() {
-return new Promise((resolve, reject) => {
-      const details = {
-      HostName: os.hostname(),
-      OperatingSystem: os.platform(),
-      Architecture: os.arch(),
-      OSRelease: os.release(),
-      Uptime: os.uptime(),
-      CPUCores: os.cpus().length,
-      TotalMemory: os.totalmem(),
-      FreeMemory: os.freemem(),
-      CurrentWorkingDirectory: process.cwd()
-      };
-      resolve(details);
-});
-}
 
-function writeJSONToFile(data) {
-      return new Promise((resolve, reject) => {
-            const json = JSON.stringify(data, null, 2);
-            fs.writeFile('os_details.json', json, 'utf8', (err) => {
-            if (err) {
-                  reject(err);
-                  return;
-            }
-            console.log("successfully written to file");
-            resolve();
-            });
-      });
-}
 
-    // Writing json file on the start of the program
-getOSDetails()
+
+/* ------ Code for storing data locally as well as to s3 bucket ------ */
+OSDetails()
       .then((details) => writeJSONToFile(details))
-      .then(() => console.log('JSON file created successfully'))
+      .then(() => console.log('JSON file created successfully', process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY))
+      .then(() => uploadTOBucket('os-details.json'))
+      .then(() => console.log(`File uploaded to S3 bucket with name ${process.env.AWS_ACCESS_KEY_ID}`))
+      .then(() => fs.unlink(process.env.FILENAME))
       .catch((error) => console.error('Error:', error));
 
 
-      const server = http.createServer((req, res) => {
-            if (req.method === 'GET' && req.url === '/get-details') {
-            fs.readFile('os_details.json', 'utf8', (err, data) => {
-                  if (err) {
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Internal Server Error');
-                        return;
-                  }
-                  res.writeHead(200, { 'Content-Type': 'text/plain' });
-                  const response = `Hello, my name is Assistant!\n\nHere is my system information:\n\n${data}`;
-                  res.end(response);
-            });
-            } else {
-                  res.writeHead(404, { 'Content-Type': 'text/plain' });
-                  res.end('Invalid request');
+/*
+const server = http.createServer((req, res) => {
+      if (req.method === 'GET' && req.url === '/get-details') {
+      fs.readFile('os_details.json', 'utf8', (err, data) => {
+            if (err) {
+                  res.writeHead(500, { 'Content-Type': 'text/plain' });
+                  res.end('Internal Server Error');
+                  return;
             }
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            const response = `Hello, my name is Assistant!\n\nHere is my system information:\n\n${data}`;
+            res.end(response);
       });
+      } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Invalid request');
+      }
+});
+*/
 
-server.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}/`);
+
+app.listen(process.env.PORT, (req, res) => {
+      console.log
+      (
+            "Server is listening on,",
+            `http://localhost:${process.env.PORT}`
+      );
+});
+
+/* there is just one route */
+// app.use("", detaisRoute);
+
+app.all("*", (req, res) => {
+      res.status(404).json({ message: "Bad Gateway" });
 });
 
 
---
+++ frontend/homework/5/ques1/route.js
@@ -0,0 +1,9 @@
+// const express = require("express")
+// const router = express.Router();
+
+// const uploadfile = require("./controller");
+
+// router.post("/upload-details", uploadfile);
+// // router.get("/get-details", getfile);
+
+// module.exports = router;
