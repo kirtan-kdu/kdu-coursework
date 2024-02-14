61ecebb12ba3a818487d1af3c71a716d6d298f8d
kirtan-kdu <kirtanrakeshkumar.shah@kickdrumtech.com>
2024-02-15 00:03:31 +0530

    Homework-7

--- frontend/homework/7/server.js
+++ frontend/homework/7/server.js
@@ -10,26 +10,27 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json())
 
-server.listen(port, () => {
-    console.log(`Server is connected: http://localhost:${port}`);
-})
-
 const io = new socketio.Server(server,{
     cors:{
-        origin:"http://127.0.0.1:5500/public/index.html"
+        origin:"http://127.0.0.1:5500"
     }
 });
+
 
 io.on("connection",(socket)=>{
 
     console.log("New user connected");
 
-    io.emit("new-message","Hey all! Someone joined KDU Chat App")
+    io.except(socket.id).emit("new-message","Hey all! Someone joined KDU Chat App")
 
     socket.on("message",(payload)=>{
         console.log("payload:",payload);
         io.except(socket.id).emit('new-message',payload);
     })
+})
+
+server.listen(port, () => {
+    console.log(`Server is connected: http://localhost:${port}`);
 })
 
 
